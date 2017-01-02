2628e1ea6a16da947c0816cb2b82dd5447afdb64
mzaid1 <DaBossZaid@Muhammads-MacBook-Pro.local>
2017-01-02 09:28:53 +0000

    Amazing ME
    Amazing Me Page Update

---
+++ Amazing_Me_files/Amazing_Me.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20170102
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Amazing_Me_files/Amazing_MeMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
